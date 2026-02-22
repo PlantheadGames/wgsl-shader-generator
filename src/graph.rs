6a2acaf42014e7afa5fad5b5e57f8367badcca06
nown992 <nick.owen992@gmail.com>
2026-02-22 18:06:18 +0000

    continued work on graph skeleton

--- src/graph.rs
+++ src/graph.rs
@@ -1,9 +1,11 @@
 
-/// This will be the file containung the graph data structure. 
+/// This will be the file containing the graph data structure. 
 /// 
 use std::collections::HashMap;
 
 type NodeId = usize;
+
+///This struct will be the end graph structure showing all the links between the ingoing and outgoing ports on the node. 
 
 #[derive(Debug)]
 struct Graph<T>{
@@ -13,11 +15,33 @@ struct Graph<T>{
     next_id: NodeId,
 }
 
-
+///This is the nodes themselves. this will be a more generic structure that the overall nodes will
+///conform. each node will store its own state that then gets passed up to the graph. 
 #[derive(Debug)]
 struct ShaderNode<T>{
     id: NodeId,
     data: T,
+}
+
+///The datatype enum will be used for the core type logic of the nodes. This will determine the
+///input or outputs of the nodes are valid.
+#[derive(Debug)]
+enum Datatype{
+    Float,
+    Vec2,
+    Vec3,
+    Vec4,
+    Mat4
+}
+
+///Define the node itself to a given type. This may be changed depending on the number of nodes to
+///
+#[derive(Debug)]
+enum NodeKind{
+    Add,
+    Multiply,
+    Divide,
+
 }
 
 impl<T> for Graph<T> {
@@ -26,6 +50,7 @@ impl<T> for Graph<T> {
             nodes: HashMap::new(),
             incoming: HashMap::new(),
             outgoing: HashMap::new(),
+            next_id: 0
         }
     }
     fn add_node(&mut self, data: T) -> NodeId{
