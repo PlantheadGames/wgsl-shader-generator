81578f08ec808a9bbd130b064d97382dca4eeef0
nown992 <nick.owen992@gmail.com>
2026-02-24 21:36:54 +0000

    cargo formatted

--- src/graph.rs
+++ src/graph.rs
@@ -1,15 +1,14 @@
-
-/// This will be the file containing the graph data structure. 
-/// 
-use std::collections::HashMap;
 use bevy::prelude::*;
+/// This will be the file containing the graph data structure.
+///
+use std::collections::HashMap;
 
 type NodeId = usize;
 
-///This struct will be the end graph structure showing all the links between the ingoing and outgoing ports on the node. 
+///This struct will be the end graph structure showing all the links between the ingoing and outgoing ports on the node.
 
 #[derive(Debug)]
-pub struct Graph{
+pub struct Graph {
     pub nodes: HashMap<NodeId, ShaderNode>,
     pub incoming_connections: HashMap<NodeId, Vec<NodeId>>,
     pub outgoing_connections: HashMap<NodeId, Vec<NodeId>>,
@@ -17,9 +16,9 @@ pub struct Graph{
 }
 
 ///This is the nodes themselves. this will be a more generic structure that the overall nodes will
-///conform. each node will store its own state that then gets passed up to the graph. 
-#[derive(Debug,Clone)]
-pub struct ShaderNode{
+///conform. each node will store its own state that then gets passed up to the graph.
+#[derive(Debug, Clone)]
+pub struct ShaderNode {
     pub incoming_connections_type: Datatype,
     pub outgoing_connections_type: Datatype,
     pub id: NodeId,
@@ -29,13 +28,12 @@ pub struct ShaderNode{
 ///The datatype enum will be used for the core type logic of the nodes. This will determine the
 ///input or outputs of the nodes are valid.
 #[derive(Debug, PartialEq, Clone)]
-pub enum Datatype{
+pub enum Datatype {
     Float,
     Vec2,
     Vec3,
     Mat4,
-    String
-
+    String,
 }
 
 //// this node will hold the internal data of the shader. This will be the user defined input. for
@@ -47,20 +45,19 @@ pub enum ShaderData {
     Vec2(Vec2),
     Vec3(Vec3),
     Mat4(Mat4),
-    String(String)
+    String(String),
 }
 ///Define the node itself to a given type. This may be changed depending on the number of nodes to
 ///
 #[derive(Debug)]
-enum _NodeKind{
+enum _NodeKind {
     Add,
     Multiply,
     Divide,
-
 }
-impl ShaderNode{
-    pub fn new(id: NodeId, data: ShaderData, incoming: Datatype, outgoing: Datatype)-> Self{
-        Self{
+impl ShaderNode {
+    pub fn new(id: NodeId, data: ShaderData, incoming: Datatype, outgoing: Datatype) -> Self {
+        Self {
             incoming_connections_type: incoming,
             outgoing_connections_type: outgoing,
             id,
@@ -69,28 +66,34 @@ impl ShaderNode{
     }
 }
 
-
 impl Graph {
     pub fn new() -> Self {
         Self {
             nodes: HashMap::new(),
             incoming_connections: HashMap::new(),
             outgoing_connections: HashMap::new(),
-            next_id: 0
+            next_id: 0,
         }
     }
-    pub fn add_node(&mut self, data: ShaderData, incoming: Datatype, outgoing: Datatype) -> NodeId{
+    pub fn add_node(&mut self, data: ShaderData, incoming: Datatype, outgoing: Datatype) -> NodeId {
         let id = self.next_id;
         self.next_id += 1;
-        self.nodes.insert(id, ShaderNode::new(id, data, incoming, outgoing));
+        self.nodes
+            .insert(id, ShaderNode::new(id, data, incoming, outgoing));
         self.incoming_connections.insert(id, Vec::new());
         self.outgoing_connections.insert(id, Vec::new());
         id
     }
     pub fn add_edge(&mut self, source_node: ShaderNode, destination_node: ShaderNode) {
-        if source_node.outgoing_connections_type == destination_node.incoming_connections_type{
-            self.outgoing_connections.get_mut(&source_node.id).unwrap().push(destination_node.id);
-            self.incoming_connections.get_mut(&destination_node.id).unwrap().push(source_node.id);
+        if source_node.outgoing_connections_type == destination_node.incoming_connections_type {
+            self.outgoing_connections
+                .get_mut(&source_node.id)
+                .unwrap()
+                .push(destination_node.id);
+            self.incoming_connections
+                .get_mut(&destination_node.id)
+                .unwrap()
+                .push(source_node.id);
         }
     }
 
@@ -102,8 +105,5 @@ impl Graph {
     pub fn get_outputs(&self, node: NodeId) -> Option<&Vec<NodeId>> {
         let vec = self.outgoing_connections.get(&node);
         vec
-
     }
 }
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,116 @@
 pub mod graph;
 pub use graph::*;
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    pub fn test_add(){
+    pub fn test_add() {
         assert_eq!(1 + 1, 2);
     }
     #[test]
-    pub fn test_inital_graph_generation(){
+    pub fn test_inital_graph_generation() {
         let mut graph = graph::Graph::new();
         assert!(graph.nodes.is_empty());
-        let _node0 = graph.add_node(ShaderData::String("this is a node".to_string()), Datatype::Vec3, Datatype::String);
+        let _node0 = graph.add_node(
+            ShaderData::String("this is a node".to_string()),
+            Datatype::Vec3,
+            Datatype::String,
+        );
         assert!(!graph.nodes.is_empty());
     }
 
-#[test]
-    pub fn test_node_connections(){
+    #[test]
+    pub fn test_node_connections() {
         let mut graph = graph::Graph::new();
         assert!(graph.nodes.is_empty());
-        
-        let node0 = graph.add_node(ShaderData::String("this is a node".to_string()), Datatype::Vec3, Datatype::String);
-        let node1 = graph.add_node(ShaderData::String("this is a second node".to_string()) , Datatype::String, Datatype::Float);
-        
-        graph.add_edge(graph.nodes.get(&node0).unwrap().clone(),graph.nodes.get(&node1).unwrap().clone());
-        
+
+        let node0 = graph.add_node(
+            ShaderData::String("this is a node".to_string()),
+            Datatype::Vec3,
+            Datatype::String,
+        );
+        let node1 = graph.add_node(
+            ShaderData::String("this is a second node".to_string()),
+            Datatype::String,
+            Datatype::Float,
+        );
+
+        graph.add_edge(
+            graph.nodes.get(&node0).unwrap().clone(),
+            graph.nodes.get(&node1).unwrap().clone(),
+        );
+
         graph.get_inputs(node1);
         graph.get_outputs(node0);
-        
-        assert_eq!(graph.nodes.get(&node0).unwrap().clone().outgoing_connections_type, graph.nodes.get(&node1).unwrap().clone().incoming_connections_type);
-    println!("{:#?}", graph);
-    }
 
+        assert_eq!(
+            graph
+                .nodes
+                .get(&node0)
+                .unwrap()
+                .clone()
+                .outgoing_connections_type,
+            graph
+                .nodes
+                .get(&node1)
+                .unwrap()
+                .clone()
+                .incoming_connections_type
+        );
+        println!("{:#?}", graph);
+    }
 }
 
-    pub fn one_add_one(){
-        assert_eq!(1 + 1, 2);
-    }
- 
-    pub fn criterion_inital_graph_generation(){
-        let mut graph = graph::Graph::new();
-        assert!(graph.nodes.is_empty());
-        let _node0 = graph.add_node(ShaderData::String("this is a node".to_string()), Datatype::Vec3, Datatype::String);
-        assert!(!graph.nodes.is_empty());
-    }
-    pub fn criterion_node_connections(){
-        let mut graph = graph::Graph::new();
-        assert!(graph.nodes.is_empty());
-        
-        let node0 = graph.add_node(ShaderData::String("this is a node".to_string()), Datatype::Vec3, Datatype::String);
-        let node1 = graph.add_node(ShaderData::String("this is a second node".to_string()) , Datatype::String, Datatype::Float);
-        
-        graph.add_edge(graph.nodes.get(&node0).unwrap().clone(),graph.nodes.get(&node1).unwrap().clone());
-        
-        graph.get_inputs(node1);
-        graph.get_outputs(node0);
-        
-        assert_eq!(graph.nodes.get(&node0).unwrap().clone().outgoing_connections_type, graph.nodes.get(&node1).unwrap().clone().incoming_connections_type);
-    }
+pub fn one_add_one() {
+    assert_eq!(1 + 1, 2);
+}
+
+pub fn criterion_inital_graph_generation() {
+    let mut graph = graph::Graph::new();
+    assert!(graph.nodes.is_empty());
+    let _node0 = graph.add_node(
+        ShaderData::String("this is a node".to_string()),
+        Datatype::Vec3,
+        Datatype::String,
+    );
+    assert!(!graph.nodes.is_empty());
+}
+pub fn criterion_node_connections() {
+    let mut graph = graph::Graph::new();
+    assert!(graph.nodes.is_empty());
+
+    let node0 = graph.add_node(
+        ShaderData::String("this is a node".to_string()),
+        Datatype::Vec3,
+        Datatype::String,
+    );
+    let node1 = graph.add_node(
+        ShaderData::String("this is a second node".to_string()),
+        Datatype::String,
+        Datatype::Float,
+    );
+
+    graph.add_edge(
+        graph.nodes.get(&node0).unwrap().clone(),
+        graph.nodes.get(&node1).unwrap().clone(),
+    );
+
+    graph.get_inputs(node1);
+    graph.get_outputs(node0);
+
+    assert_eq!(
+        graph
+            .nodes
+            .get(&node0)
+            .unwrap()
+            .clone()
+            .outgoing_connections_type,
+        graph
+            .nodes
+            .get(&node1)
+            .unwrap()
+            .clone()
+            .incoming_connections_type
+    );
+}
